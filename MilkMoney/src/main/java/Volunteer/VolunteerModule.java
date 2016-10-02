42e8207eb470946b0e4446335c0955c04d09638b
calebripley <calebaripley@gmail.com>
2016-10-02 13:45:41 -0700

    Supposed to be final commit

--- MilkMoney/src/main/java/Volunteer/VolunteerModule.java
+++ MilkMoney/src/main/java/Volunteer/VolunteerModule.java
@@ -28,210 +28,204 @@ import java.util.List;
  */
 public class VolunteerModule extends Application {
 
-  TextArea broadCastMessageTextArea;
-  TextField emailAddressTextField;
-  PasswordField emailPasswordTextField;
-  TextField subjectField;
-  TextField fromField;
+    TextArea broadCastMessageTextArea;
+    TextField emailAddressTextField;
+    PasswordField emailPasswordTextField;
+    TextField subjectField;
+    TextField fromField;
 
-  public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
 
-    BorderPane volunteerApplicationPane = new BorderPane();
+        BorderPane volunteerApplicationPane = new BorderPane();
 
-    ArrayList<String> volunteerNameList = DatabaseConnection.getVolunteerName();
-    ArrayList<String> volunteerEmailList = DatabaseConnection.volunteerEmailList();
-    ArrayList<String> volunteerPhoneList = DatabaseConnection.volunteerPhoneList();
-    ArrayList<String> donorEmailList = DatabaseConnection.donorEmailList();
-    ArrayList<String> donorPhoneList = DatabaseConnection.donorPhoneList();
+        ArrayList<String> volunteerNameList = DatabaseConnection.getVolunteerName();
+        ArrayList<String> volunteerEmailList = DatabaseConnection.volunteerEmailList();
+        ArrayList<String> volunteerPhoneList = DatabaseConnection.volunteerPhoneList();
+        ArrayList<String> donorEmailList = DatabaseConnection.donorEmailList();
+        ArrayList<String> donorPhoneList = DatabaseConnection.donorPhoneList();
 
-    volunteerApplicationPane.setTop(broadcastTitleAndTextBox());
-    volunteerApplicationPane.setBottom(sendBroadCastsBox());
-    volunteerApplicationPane.setLeft(checkListWrapperBox(volunteerNameList));
-    volunteerApplicationPane.setRight(groupsWrapperGridBox(volunteerEmailList));
+        volunteerApplicationPane.setTop(broadcastTitleAndTextBox());
+        volunteerApplicationPane.setBottom(sendBroadCastsBox(volunteerEmailList, volunteerPhoneList));
+        volunteerApplicationPane.setLeft(checkListWrapperBox(volunteerNameList));
+        volunteerApplicationPane.setRight(groupsWrapperGridBox());
 
-    primaryStage.setTitle("MILK VOLUNTEER SYSTEM");
-    Scene scene = new Scene(volunteerApplicationPane, 700, 400);
-    primaryStage.setScene(scene);
-    primaryStage.show();
-    volunteerApplicationPane.requestFocus();
-  }
+        primaryStage.setTitle("MILK VOLUNTEER SYSTEM");
+        Scene scene = new Scene(volunteerApplicationPane, 700, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        volunteerApplicationPane.requestFocus();
+    }
 
-  private VBox groupsWrapperGridBox(ArrayList<String> volunteerEmailList) {
+    private VBox groupsWrapperGridBox() {
 
-    VBox emailMessageGridBox = new VBox();
-    emailMessageGridBox.setPrefWidth(400);
+        VBox emailMessageGridBox = new VBox();
+        emailMessageGridBox.setPrefWidth(400);
 
-    VBox subjectVBox = new VBox();
-    HBox emailHBox = new HBox();
-    emailHBox.setPrefWidth(400);
-    emailHBox.setPadding(new Insets(10));
-    emailHBox.setSpacing(5);
-    Label subjectLabel = new Label("SUBJECT: ");
-    subjectField = new TextField();
-    subjectField.setPrefWidth(268);
-    subjectField.setPromptText("Enter subject here");
-    emailHBox.getChildren().addAll(subjectLabel, subjectField);
+        VBox subjectVBox = new VBox();
+        HBox emailHBox = new HBox();
+        emailHBox.setPrefWidth(400);
+        emailHBox.setPadding(new Insets(10));
+        emailHBox.setSpacing(5);
+        Label subjectLabel = new Label("SUBJECT: ");
+        subjectField = new TextField();
+        subjectField.setPrefWidth(268);
+        subjectField.setPromptText("Enter subject here");
+        emailHBox.getChildren().addAll(subjectLabel, subjectField);
 
-    HBox emailAndPassHBox = new HBox();
-    emailAndPassHBox.setPrefWidth(400);
-    emailAndPassHBox.setPadding(new Insets(10));
-    emailAndPassHBox.setSpacing(5);
-    emailAddressTextField = new TextField();
-    emailAddressTextField.setPromptText("Enter email address");
-    emailPasswordTextField = new PasswordField();
-    emailPasswordTextField.setPromptText("Enter Password");
-    emailAndPassHBox.getChildren().addAll(emailAddressTextField, emailPasswordTextField);
+        HBox emailAndPassHBox = new HBox();
+        emailAndPassHBox.setPrefWidth(400);
+        emailAndPassHBox.setPadding(new Insets(10));
+        emailAndPassHBox.setSpacing(5);
+        emailAddressTextField = new TextField();
+        emailAddressTextField.setPromptText("Enter email address");
+        emailPasswordTextField = new PasswordField();
+        emailPasswordTextField.setPromptText("Enter Password");
+        emailAndPassHBox.getChildren().addAll(emailAddressTextField, emailPasswordTextField);
 
-    subjectVBox.getChildren().addAll(emailHBox, emailAndPassHBox);
+        subjectVBox.getChildren().addAll(emailHBox, emailAndPassHBox);
 
-    HBox textMessageHBox = new HBox();
-    textMessageHBox.setPrefWidth(400);
-    textMessageHBox.setPadding(new Insets(10));
-    textMessageHBox.setSpacing(5);
-    Label fromLabel = new Label("PHONE NUMBER: ");
-    fromField = new TextField();
-    fromField.setPromptText("Enter number here");
-    fromField.setText("12015834652");
-    textMessageHBox.getChildren().addAll(fromLabel, fromField);
+        HBox textMessageHBox = new HBox();
+        textMessageHBox.setPrefWidth(400);
+        textMessageHBox.setPadding(new Insets(10));
+        textMessageHBox.setSpacing(5);
+        Label fromLabel = new Label("PHONE NUMBER: ");
+        fromField = new TextField();
+        fromField.setPromptText("Enter number here");
+        fromField.setText("12015834652");
+        textMessageHBox.getChildren().addAll(fromLabel, fromField);
 
-    GridPane platformsWrapperGridPane = new GridPane();
+        GridPane platformsWrapperGridPane = new GridPane();
 
-    platformsWrapperGridPane.setHgap(10);
-    platformsWrapperGridPane.setVgap(10);
-    platformsWrapperGridPane.setPadding(new Insets(0, 10, 0, 10));
-    platformsWrapperGridPane.setPrefWidth(400);
-    platformsWrapperGridPane.setAlignment(Pos.CENTER);
+        platformsWrapperGridPane.setHgap(10);
+        platformsWrapperGridPane.setVgap(10);
+        platformsWrapperGridPane.setPadding(new Insets(0, 10, 0, 10));
+        platformsWrapperGridPane.setPrefWidth(400);
+        platformsWrapperGridPane.setAlignment(Pos.CENTER);
 
-    // Category in column 1, row 1
-    CheckBox neighborsBox = new CheckBox("Neighbors");
-    platformsWrapperGridPane.add(neighborsBox, 0, 0);
+        // Category in column 1, row 1
+        CheckBox neighborsBox = new CheckBox("Neighbors");
+        platformsWrapperGridPane.add(neighborsBox, 0, 0);
 
-    CheckBox servicesBox = new CheckBox("Services");
-    platformsWrapperGridPane.add(servicesBox, 0, 1);
+        CheckBox servicesBox = new CheckBox("Services");
+        platformsWrapperGridPane.add(servicesBox, 0, 1);
 
-    CheckBox mentorsBox = new CheckBox("Mentors");
-    platformsWrapperGridPane.add(mentorsBox, 1, 0);
+        CheckBox mentorsBox = new CheckBox("Mentors");
+        platformsWrapperGridPane.add(mentorsBox, 1, 0);
 
-    CheckBox otherBox = new CheckBox("Others");
-    platformsWrapperGridPane.add(otherBox, 1, 1);
+        CheckBox otherBox = new CheckBox("Others");
+        platformsWrapperGridPane.add(otherBox, 1, 1);
 
-    CheckBox allBox = new CheckBox("All Volunteers");
-    platformsWrapperGridPane.add(allBox, 0, 2);
+        CheckBox allBox = new CheckBox("All Volunteers");
+        platformsWrapperGridPane.add(allBox, 0, 2);
 
-    emailMessageGridBox.getChildren().addAll(subjectVBox, textMessageHBox, platformsWrapperGridPane);
+        emailMessageGridBox.getChildren().addAll(subjectVBox, textMessageHBox, platformsWrapperGridPane);
 
-    return emailMessageGridBox;
-  }
+        return emailMessageGridBox;
+    }
 
-  private ScrollPane checkListWrapperBox(ArrayList<String> volunteerNameList) {
+    private ScrollPane checkListWrapperBox(ArrayList<String> volunteerNameList) {
 
-    VBox checkListWrapperBox = new VBox();
-    checkListWrapperBox.setPadding(new Insets(10));
-    checkListWrapperBox.setSpacing(5);
+        VBox checkListWrapperBox = new VBox();
+        checkListWrapperBox.setPadding(new Insets(10));
+        checkListWrapperBox.setSpacing(5);
 
-    List<CheckBox> checkBoxList = new ArrayList<CheckBox>();
+        List<CheckBox> checkBoxList = new ArrayList<CheckBox>();
 
-    CheckBox cb1 = new CheckBox("First");
-    CheckBox cb2 = new CheckBox("Second");
-    CheckBox cb3 = new CheckBox("Third");
-    cb1.setSelected(true);
-    cb3.setSelected(true);
+        for (int i = 0; i < volunteerNameList.size(); i++) {
+            CheckBox newCb = new CheckBox(volunteerNameList.get(i));
+            newCb.setSelected(true);
+            checkBoxList.add(newCb);
+        }
 
-    checkBoxList.add(cb1);
-    checkBoxList.add(cb2);
-    checkBoxList.add(cb3);
-    checkBoxList.add(new CheckBox("Fourth"));
-    checkBoxList.add(new CheckBox("Fourth"));
+        checkListWrapperBox.getChildren().addAll(checkBoxList);
 
-    checkListWrapperBox.getChildren().addAll(checkBoxList);
+        ScrollPane scrollPane = new ScrollPane(checkListWrapperBox);
+        scrollPane.setPrefWidth(300);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setStyle("-fx-background-color:transparent;");
 
-    ScrollPane scrollPane = new ScrollPane(checkListWrapperBox);
-    scrollPane.setPrefWidth(300);
-    scrollPane.setFitToHeight(true);
-    scrollPane.setStyle("-fx-background-color:transparent;");
+        return scrollPane;
+    }
 
-    return scrollPane;
-  }
+    private HBox sendBroadCastsBox(final ArrayList<String> volunteerEmailList, final ArrayList<String> volunteerPhoneList) {
 
-  private HBox sendBroadCastsBox() {
+        HBox sendBroadCastBox = new HBox();
+        sendBroadCastBox.setAlignment(Pos.CENTER);
+        sendBroadCastBox.setPadding(new Insets(20));
+        sendBroadCastBox.setSpacing(10);
 
-    HBox sendBroadCastBox = new HBox();
-    sendBroadCastBox.setAlignment(Pos.CENTER);
-    sendBroadCastBox.setPadding(new Insets(20));
-    sendBroadCastBox.setSpacing(10);
+        Button submitButton = new Button("SEND");
+        submitButton.setPrefSize(100, 20);
+        submitButton.setOnAction(new EventHandler<ActionEvent>() {
 
-    Button submitButton = new Button("SEND");
-    submitButton.setPrefSize(100, 20);
-    submitButton.setOnAction(new EventHandler<ActionEvent>() {
-
-      public void handle(ActionEvent event) {
+            public void handle(ActionEvent event) {
 //        System.out.println("SUBMITTED!!");
 
-        if (broadCastMessageTextArea.getText() != "") {
-          // Create Text Message
+                if (broadCastMessageTextArea.getText() != "") {
+                    // Create Text Message
 
-          String message = broadCastMessageTextArea.getText();
-          String emailAddress = emailAddressTextField.getText();
-          String emailPassword = emailPasswordTextField.getText();
-          String subject = subjectField.getText();
-          String from = fromField.getText();
-          sendEmails(emailAddress, emailPassword, subject, message);
-          sendTextMessages(from, subject, message);
-          broadCastMessageTextArea.clear();
+                    String message = broadCastMessageTextArea.getText();
+                    String emailAddress = emailAddressTextField.getText();
+                    String emailPassword = emailPasswordTextField.getText();
+                    String subject = subjectField.getText();
+                    String from = fromField.getText();
+                    sendEmails(emailAddress, emailPassword, subject, message, volunteerEmailList);
+                    sendTextMessages(from, subject, message, volunteerPhoneList);
+                    broadCastMessageTextArea.clear();
 
-          // Pop up to confirm message was sent
+                    // Pop up to confirm message was sent
 
-        }
-      }
-    });
+                }
+            }
+        });
 
-    sendBroadCastBox.getChildren().add(submitButton);
+        sendBroadCastBox.getChildren().add(submitButton);
 
-    return sendBroadCastBox;
-  }
+        return sendBroadCastBox;
+    }
 
-  private void sendTextMessages(String from, String subject, String message) {
+    private void sendTextMessages(String from, String subject, String message, ArrayList<String> volunteerPhoneList) {
 
-    ArrayList<String> toNums = new ArrayList<String>();
-    toNums.add("14802269800");
+        ArrayList<String> toNums = new ArrayList<String>();
+        toNums.addAll(volunteerPhoneList);
 //    String from = "12015834652";
-    TextMessageAdapter text = new TextMessageAdapter(toNums, from, subject + ": " + message);
-    text.sendText();
-  }
+        TextMessageAdapter text = new TextMessageAdapter(toNums, from, subject + ": " + message);
+        text.sendText();
+    }
 
-  private void sendEmails(String emailAddress, String pass, String subject, String message) {
+    private void sendEmails(String emailAddress, String pass, String subject, String message, ArrayList<String> volunteerEmailList) {
 
-    // Create Email Message
+        // Create Email Message
 //    String senderEmail = "milkmoneyone@gmail.com";
 //    String senderPassword = "milkmoney1";
-    ArrayList<String> toEmails = new ArrayList<String>();
-    toEmails.add("calebaripley@gmail.com");
-    EmailMessageAdapter email = new EmailMessageAdapter(emailAddress, pass, toEmails, subject, message);
-    email.sendEmail();
+        ArrayList<String> toEmails = new ArrayList<String>();
+        toEmails.addAll(volunteerEmailList);
+        EmailMessageAdapter email = new EmailMessageAdapter(emailAddress, pass, toEmails, subject, message);
+        email.sendEmail();
 
-  }
+    }
 
-  public HBox broadcastTitleAndTextBox() {
+    public HBox broadcastTitleAndTextBox() {
 
-    VBox bannerVerticalWrapper = new VBox();
-    bannerVerticalWrapper.setPadding(new Insets(2));
-    bannerVerticalWrapper.setSpacing(10);
+        VBox bannerVerticalWrapper = new VBox();
+        bannerVerticalWrapper.setPadding(new Insets(2));
+        bannerVerticalWrapper.setSpacing(10);
 
-    Text broadcastMessageTitle = new Text("ENTER YOUR MESSAGE");
-    broadcastMessageTitle.setFont(Font.font("Arial", FontWeight.BOLD, 14));
-    broadcastMessageTitle.setFill(Color.WHITE);
+        Text broadcastMessageTitle = new Text("ENTER YOUR MESSAGE");
+        broadcastMessageTitle.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+        broadcastMessageTitle.setFill(Color.WHITE);
 
-    broadCastMessageTextArea = new TextArea();
-    broadCastMessageTextArea.setPrefSize(800, 50);
-    bannerVerticalWrapper.getChildren().addAll(broadcastMessageTitle, broadCastMessageTextArea);
+        broadCastMessageTextArea = new TextArea();
+        broadCastMessageTextArea.setPrefSize(800, 50);
+        bannerVerticalWrapper.getChildren().addAll(broadcastMessageTitle, broadCastMessageTextArea);
 
-    HBox broadcastTitleAndMessageWrapper = new HBox();
-    broadcastTitleAndMessageWrapper.setPadding(new Insets(5, 5, 5, 5));
-    broadcastTitleAndMessageWrapper.setSpacing(3);
-    broadcastTitleAndMessageWrapper.setStyle("-fx-background-color: #B34EB3;");
+        HBox broadcastTitleAndMessageWrapper = new HBox();
+        broadcastTitleAndMessageWrapper.setPadding(new Insets(5, 5, 5, 5));
+        broadcastTitleAndMessageWrapper.setSpacing(3);
+        broadcastTitleAndMessageWrapper.setStyle("-fx-background-color: #B34EB3;");
 
-    broadcastTitleAndMessageWrapper.getChildren().addAll(bannerVerticalWrapper);
+        broadcastTitleAndMessageWrapper.getChildren().addAll(bannerVerticalWrapper);
 
-    return broadcastTitleAndMessageWrapper;
-  }
+        return broadcastTitleAndMessageWrapper;
+    }
 }
--- MilkMoney/src/main/java/utility/DatabaseConnection.java
+++ MilkMoney/src/main/java/utility/DatabaseConnection.java
@@ -11,123 +11,123 @@ import java.util.List;
 // Sample way to create a database connection
 public class DatabaseConnection {
 
-  private static ArrayList<String> volunteerNameString;
-  private static ArrayList<String> donorNameString;
-  private static  ArrayList<String> donorEmailList;
-  private static  ArrayList<String> volunteerEmailList;
-  private static  ArrayList<String> volunteerPhoneList;
-  private static  ArrayList<String> donorPhoneList;
+    private static ArrayList<String> volunteerNameString;
+    private static ArrayList<String> donorNameString;
+    private static ArrayList<String> donorEmailList;
+    private static ArrayList<String> volunteerEmailList;
+    private static ArrayList<String> volunteerPhoneList;
+    private static ArrayList<String> donorPhoneList;
 
-  public static void init() throws SQLException, ClassNotFoundException {
-    Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+    public static void init() throws SQLException, ClassNotFoundException {
+        Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 
-    Connection conn = DriverManager.getConnection(
-            "jdbc:ucanaccess://C:\\Users\\Anshuman\\Desktop\\db.accdb");
-    Statement s = conn.createStatement();
+        Connection conn = DriverManager.getConnection(
+                "jdbc:ucanaccess://C:\\Users\\Anshuman\\Desktop\\db.accdb");
+        Statement s = conn.createStatement();
 
 		/*Query for selecting Id*/
-    ResultSet rsDonorsId = s.executeQuery("SELECT [Id] FROM [Donors] where [Donor] is [TRUE]");
-    ResultSet rsVolunteersId = s.executeQuery("SELECT [Id] FROM [Donors] where [Volunteer] is [TRUE]");
+        ResultSet rsDonorsId = s.executeQuery("SELECT [Id] FROM [Donors] where [Donor] is [TRUE]");
+        ResultSet rsVolunteersId = s.executeQuery("SELECT [Id] FROM [Donors] where [Volunteer] is [TRUE]");
 
-    donorNameString = new ArrayList<String>();
-    volunteerNameString = new ArrayList<String>();
-    volunteerEmailList = new ArrayList<String>();
-    donorEmailList = new ArrayList<String>();
-    volunteerPhoneList = new ArrayList<String>();
-    donorPhoneList = new ArrayList<String>();
+        donorNameString = new ArrayList<String>();
+        volunteerNameString = new ArrayList<String>();
+        volunteerEmailList = new ArrayList<String>();
+        donorEmailList = new ArrayList<String>();
+        volunteerPhoneList = new ArrayList<String>();
+        donorPhoneList = new ArrayList<String>();
 
-    while (rsDonorsId.next()) {
+        while (rsDonorsId.next()) {
 
-      String temp = rsDonorsId.getString(1);
-      int i = Integer.parseInt(temp);
-      ResultSet rsDonorsName = s.executeQuery("SELECT [FirstName] FROM [Donors] where [Id]=" + i + " ");
+            String temp = rsDonorsId.getString(1);
+            int i = Integer.parseInt(temp);
+            ResultSet rsDonorsName = s.executeQuery("SELECT [FirstName] FROM [Donors] where [Id]=" + i + " ");
 
-      while (rsDonorsName.next()) {
-        donorNameString.add(rsDonorsName.getString(1));
-      }
-    }
+            while (rsDonorsName.next()) {
+                donorNameString.add(rsDonorsName.getString(1));
+            }
+        }
 
-    while (rsVolunteersId.next()) {
+        while (rsVolunteersId.next()) {
 
-      String temp2 = rsVolunteersId.getString(1);
-      int i2 = Integer.parseInt(temp2);
-      ResultSet rsVolunteersName = s.executeQuery("SELECT [FirstName] FROM [Donors] where [Id]=" + i2 + " ");
+            String temp2 = rsVolunteersId.getString(1);
+            int i2 = Integer.parseInt(temp2);
+            ResultSet rsVolunteersName = s.executeQuery("SELECT [FirstName] FROM [Donors] where [Id]=" + i2 + " ");
 
-      while (rsVolunteersName.next()) {
-        volunteerNameString.add(rsVolunteersName.getString(1));
-      }
-    }
+            while (rsVolunteersName.next()) {
+                volunteerNameString.add(rsVolunteersName.getString(1));
+            }
+        }
 
-    //		/*Query for selecting Phone number matching to name[Id]*/
+        //		/*Query for selecting Phone number matching to name[Id]*/
 
 		/**/
 
-    ResultSet rsDonorsId2 = s.executeQuery("SELECT [Id] FROM [Donors] where [Donor] is [TRUE]");
-    ResultSet rsVolunteersId2 = s.executeQuery("SELECT [Id] FROM [Donors] where [Volunteer] is [TRUE]");
+        ResultSet rsDonorsId2 = s.executeQuery("SELECT [Id] FROM [Donors] where [Donor] is [TRUE]");
+        ResultSet rsVolunteersId2 = s.executeQuery("SELECT [Id] FROM [Donors] where [Volunteer] is [TRUE]");
 
-    while (rsDonorsId2.next()) {
+        while (rsDonorsId2.next()) {
 
-      String temp3 = rsDonorsId2.getString(1);
+            String temp3 = rsDonorsId2.getString(1);
 //      System.out.println(temp3);
-      int i3 = Integer.parseInt(temp3);
-      ResultSet rsDonorPhoneMatch = s.executeQuery("SELECT [MobilePhone] FROM [Donors] where ID=" + i3 + "");
-      ResultSet rsDonorMailMatch = s.executeQuery("SELECT [EmailAddress] FROM [Donors] where ID=" + i3 + "");
-      while (rsDonorPhoneMatch.next()) {
-        donorPhoneList.add(rsDonorPhoneMatch.getString(1));
+            int i3 = Integer.parseInt(temp3);
+            ResultSet rsDonorPhoneMatch = s.executeQuery("SELECT [MobilePhone] FROM [Donors] where ID=" + i3 + "");
+            ResultSet rsDonorMailMatch = s.executeQuery("SELECT [EmailAddress] FROM [Donors] where ID=" + i3 + "");
+            while (rsDonorPhoneMatch.next()) {
+                donorPhoneList.add(rsDonorPhoneMatch.getString(1));
 
-      }
-      while (rsDonorMailMatch.next()) {
+            }
+            while (rsDonorMailMatch.next()) {
 
-        donorEmailList.add(rsDonorMailMatch.getString(1));
+                donorEmailList.add(rsDonorMailMatch.getString(1));
 
-      }
+            }
+        }
+
+
+        while (rsVolunteersId2.next()) {
+
+            String temp4 = rsVolunteersId2.getString(1);
+            int i4 = Integer.parseInt(temp4);
+            ResultSet rsVolunteersPhoneMatch = s.executeQuery("SELECT [MobilePhone] FROM [Donors] where ID=" + i4 + "");
+            ResultSet rsVolunteersMailMatch = s.executeQuery("SELECT [EmailAddress] FROM [Donors] where ID=" + i4 + "");
+
+            while (rsVolunteersPhoneMatch.next()) {
+                volunteerPhoneList.add(rsVolunteersPhoneMatch.getString(1));
+
+            }
+            while (rsVolunteersMailMatch.next()) {
+                volunteerEmailList.add(rsVolunteersMailMatch.getString(1));
+
+            }
+
+        }
+
+
     }
 
-
-    while (rsVolunteersId2.next()) {
-
-      String temp4 = rsVolunteersId2.getString(1);
-      int i4 = Integer.parseInt(temp4);
-      ResultSet rsVolunteersPhoneMatch = s.executeQuery("SELECT [MobilePhone] FROM [Donors] where ID=" + i4 + "");
-      ResultSet rsVolunteersMailMatch = s.executeQuery("SELECT [EmailAddress] FROM [Donors] where ID=" + i4 + "");
-
-      while (rsVolunteersPhoneMatch.next()) {
-        volunteerPhoneList.add(rsVolunteersPhoneMatch.getString(1));
-
-      }
-      while (rsVolunteersMailMatch.next()) {
-        volunteerEmailList.add(rsVolunteersMailMatch.getString(1));
-
-      }
-
+    public static ArrayList<String> getVolunteerName() throws SQLException, ClassNotFoundException {
+        init();
+        return volunteerNameString;
     }
 
+    public static ArrayList<String> getDonorName() {
+        return donorNameString;
+    }
 
+    public static ArrayList<String> donorPhoneList() {
+        return donorPhoneList;
+    }
 
+    public static ArrayList<String> volunteerEmailList() {
+        return volunteerEmailList;
+    }
 
+    public static ArrayList<String> volunteerPhoneList() {
+        return volunteerPhoneList;
+    }
 
-  }
-
-  public static ArrayList<String> getVolunteerName() throws SQLException, ClassNotFoundException {
-    init();
-    return volunteerNameString;
-  }
-
-  public static ArrayList<String> getDonorName() {
-    return donorNameString;
-  }
-
-  public static ArrayList<String> donorPhoneList() {
-    return donorPhoneList;
-  }
-  public static ArrayList<String> volunteerEmailList() {
-    return volunteerEmailList;
-  }
-  public static ArrayList<String> volunteerPhoneList() {
-    return volunteerPhoneList;
-  }
-  public static ArrayList<String> donorEmailList() {
-    return donorEmailList;
-  }
+    public static ArrayList<String> donorEmailList() {
+        return donorEmailList;
+    }
 
 }
